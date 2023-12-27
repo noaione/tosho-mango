479f3b8407a0292f900ae473b03e7bd0ba1c8a8b
noaione <noaione0809@gmail.com>
2023-12-27 12:44:53 +0700

    feat(kmkc): bootstrap models and client

--- tosho_kmkc/src/lib.rs
+++ tosho_kmkc/src/lib.rs
@@ -2,3 +2,19 @@ pub use config::*;
 pub mod config;
 pub mod constants;
 pub mod imaging;
+pub mod models;
+
+#[derive(Debug)]
+pub struct KMClient {
+    inner: reqwest::Client,
+    config: KMConfig,
+}
+
+impl KMClient {
+    pub fn new(config: KMConfig) -> Self {
+        Self {
+            inner: reqwest::Client::new(),
+            config,
+        }
+    }
+}
