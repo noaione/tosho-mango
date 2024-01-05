d6e439b93987366060bb51f1b45a29cd8182cc7a
noaione <noaione0809@gmail.com>
2024-01-05 16:10:27 +0700

    refactor: adjust CLI name and disable help subcmd

--- tosho/src/cli.rs
+++ tosho/src/cli.rs
@@ -112,7 +112,10 @@ impl WeeklyCodeCli {
 }
 
 #[derive(Parser)]
+#[command(name = "tosho")]
+#[command(bin_name = "tosho")]
 #[command(author, version, about, long_about = None, styles = cli_styles())]
+#[command(propagate_version = true, disable_help_subcommand = true)]
 pub(crate) struct ToshoCli {
     /// Increase message verbosity
     #[arg(short, long, action = clap::ArgAction::Count)]
