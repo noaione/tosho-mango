261082751fed20b815504cda9b432f919880a199
noaione <noaione0809@gmail.com>
2024-02-18 10:06:17 +0700

    fix(docs/rbean): wrong crate name used in doctest
    copy/paste strikes again

--- tosho_rbean/src/constants.rs
+++ tosho_rbean/src/constants.rs
@@ -3,7 +3,7 @@
 //! All the following structs are a lazy static.
 //!
 //! ```rust
-//! use tosho_amap::constants::get_constants;
+//! use tosho_rbean::constants::get_constants;
 //!
 //! let _ = get_constants(1); // Web
 //! ```
@@ -84,7 +84,7 @@ lazy_static! {
 ///
 /// # Examples
 /// ```
-/// use tosho_amap::constants::get_constants;
+/// use tosho_rbean::constants::get_constants;
 ///
 /// let _ = get_constants(1); // Web
 /// ```
