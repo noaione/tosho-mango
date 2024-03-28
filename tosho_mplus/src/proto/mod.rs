6a49fbaebdb8406b0f6334e28953031c6ea9e5b1
noaione <noaione0809@gmail.com>
2024-03-28 12:37:12 +0700

    fix(mplus): put SuccessOrError data with Boxed
    Signed-off-by: noaione <noaione0809@gmail.com>

--- tosho_mplus/src/proto/mod.rs
+++ tosho_mplus/src/proto/mod.rs
@@ -10,20 +10,31 @@ pub mod enums;
 pub use common::*;
 pub use enums::*;
 
+/// Indicate a success response
+///
+/// Depending on the request type, not all field will be available.
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct SuccessResponse {
     #[prost(uint64, tag = "1")]
     pub stub: u64,
 }
 
+/// A success or error response enum
+///
+/// This is used like `oneOf` in Protobuf so only one of them
+/// will be available.
+///
+/// TODO:
+/// - Remove [`Box`] when all message type are implemented in [`SuccessResponse`]
 #[derive(Clone, PartialEq, ::prost::Oneof)]
 pub enum SuccessOrError {
     #[prost(message, tag = "1")]
-    Success(SuccessResponse),
+    Success(Box<SuccessResponse>),
     #[prost(message, tag = "2")]
-    Error(ErrorResponse),
+    Error(Box<ErrorResponse>),
 }
 
+/// Proto response from the API, wrap a simple `oneOf` of [`SuccessOrError`]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Response {
     #[prost(oneof = "SuccessOrError", tags = "1, 2")]
