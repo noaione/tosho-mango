0d083437626ae451363d0781435d60053e9b3dd5
noaione <noaione0809@gmail.com>
2024-10-11 07:37:00 +0700

    refactor(macros): move impl into separate files

---
+++ tosho_macros/src/deser.rs
@@ -0,0 +1,133 @@
+use proc_macro::TokenStream;
+
+pub(crate) fn impl_serenum_derive(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    match &ast.data {
+        syn::Data::Enum(_) => {}
+        _ => panic!("`SerializeEnum` can only be derived for enums"),
+    };
+
+    let tokens = quote::quote! {
+        impl Serialize for #name {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                serializer.serialize_str(&self.to_string())
+            }
+        }
+    };
+    tokens.into()
+}
+
+pub(crate) fn impl_deserenum_derive(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    match &ast.data {
+        syn::Data::Enum(_) => {}
+        _ => panic!("`DeserializeEnum` can only be derived for enums"),
+    };
+
+    let tokens = quote::quote! {
+        impl<'de> Deserialize<'de> for #name {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                let s = String::deserialize(deserializer)?;
+                s.parse::<#name>().map_err(serde::de::Error::custom)
+            }
+        }
+    };
+    tokens.into()
+}
+
+pub(crate) fn impl_serenum32_derive(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+
+    // We want to get the values of the enum variants to create our match arms
+    let variants = match &ast.data {
+        syn::Data::Enum(v) => &v.variants,
+        _ => panic!("`SerializeEnum32` can only be derived for enums"),
+    };
+
+    let mut match_arms = vec![];
+    for variant in variants {
+        let variant_name = &variant.ident;
+        let value = if let Some((_, expr)) = &variant.discriminant {
+            quote::quote! { #expr }
+        } else {
+            quote::quote! { stringify!(#variant_name).parse().unwrap() }
+        };
+
+        match_arms.push(quote::quote! {
+            #name::#variant_name => serializer.serialize_i32(#value),
+        });
+    }
+
+    let tokens = quote::quote! {
+        impl Serialize for #name {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: serde::Serializer,
+            {
+                // serialize to i32
+                match self {
+                    #(#match_arms)*
+                }
+            }
+        }
+    };
+    tokens.into()
+}
+
+pub(crate) fn impl_deserenum32_derive(ast: &syn::DeriveInput, with_default: bool) -> TokenStream {
+    let name = &ast.ident;
+
+    // We want to get the values of the enum variants to create our match arms
+    let variants = match &ast.data {
+        syn::Data::Enum(v) => &v.variants,
+        _ => panic!("`DeserializeEnum32` can only be derived for enums"),
+    };
+
+    let mut match_arms = vec![];
+    for variant in variants {
+        let variant_name = &variant.ident;
+        let value = if let Some((_, expr)) = &variant.discriminant {
+            quote::quote! { #expr }
+        } else {
+            quote::quote! { stringify!(#variant_name).parse().unwrap() }
+        };
+
+        match_arms.push(quote::quote! {
+            #value => Ok(#name::#variant_name),
+        });
+    }
+
+    match with_default {
+        true => {
+            match_arms.push(quote::quote! {
+                _ => Ok(#name::default()),
+            });
+        }
+        false => {
+            match_arms.push(quote::quote! {
+                _ => Err(serde::de::Error::custom(format!("Invalid {} value: {}", stringify!(#name), s))),
+            })
+        }
+    }
+
+    let tokens = quote::quote! {
+        impl<'de> Deserialize<'de> for #name {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                let s = i32::deserialize(deserializer)?;
+                match s {
+                    #(#match_arms)*
+                }
+            }
+        }
+    };
+    tokens.into()
+}
---
+++ tosho_macros/src/enums.rs
@@ -0,0 +1,139 @@
+use proc_macro::TokenStream;
+
+pub(crate) fn impl_enumname_derive(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    // check if ast is an enum
+    let variants = match &ast.data {
+        syn::Data::Enum(v) => &v.variants,
+        _ => panic!("`EnumName` can only be derived for enums"),
+    };
+
+    let mut arms = Vec::new();
+    for variant in variants {
+        let ident = &variant.ident;
+        arms.push(quote::quote! {
+            Self::#ident => stringify!(#ident),
+        });
+    }
+
+    let tokens = quote::quote! {
+        impl #name {
+            /// Returns the name of the enum variant as a string
+            pub fn to_name(&self) -> &'static str {
+                match self {
+                    #(#arms)*
+                }
+            }
+        }
+    };
+    tokens.into()
+}
+
+pub(crate) fn impl_enumcount_derive(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+
+    let variants = match &ast.data {
+        syn::Data::Enum(v) => &v.variants,
+        _ => panic!("`EnumCount` can only be derived for enums"),
+    };
+
+    let count = variants.len();
+
+    let tokens = quote::quote! {
+        impl #name {
+            /// Returns the number of variants in the enum
+            pub fn count() -> usize {
+                #count
+            }
+        }
+    };
+    tokens.into()
+}
+
+pub(crate) fn impl_enumu32_derive(ast: &syn::DeriveInput, with_default: bool) -> TokenStream {
+    let name = &ast.ident;
+
+    let variants = match &ast.data {
+        syn::Data::Enum(v) => &v.variants,
+        _ => panic!("`EnumU32` can only be derived for enums"),
+    };
+
+    let mut match_arms = vec![];
+    for variant in variants {
+        let variant_name = &variant.ident;
+        // convert from u32 to enum
+        let value = if let Some((_, expr)) = &variant.discriminant {
+            quote::quote! { #expr }
+        } else {
+            quote::quote! { stringify!(#variant_name).parse().unwrap() }
+        };
+
+        match_arms.push(quote::quote! {
+            #value => #name::#variant_name,
+        });
+    }
+
+    match with_default {
+        true => {
+            match_arms.push(quote::quote! {
+                _ => #name::default(),
+            });
+        }
+        false => match_arms.push(quote::quote! {
+            _ => panic!("Invalid value for {}: {}", stringify!(#name), value)
+        }),
+    }
+
+    let tokens = quote::quote! {
+        impl From<u32> for #name {
+            fn from(value: u32) -> Self {
+                match value {
+                    #(#match_arms)*
+                }
+            }
+        }
+    };
+    tokens.into()
+}
+
+pub(crate) struct EnumErrorMacroInput {
+    name: syn::Ident,
+}
+
+impl syn::parse::Parse for EnumErrorMacroInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        Ok(Self {
+            name: input.parse()?,
+        })
+    }
+}
+
+pub(crate) fn impl_enum_error(ast: &EnumErrorMacroInput) -> TokenStream {
+    // enum_error
+    let name = &ast.name;
+    let stripped_name = name.to_string();
+    let stripped_name = stripped_name.strip_suffix("FromStrError").unwrap();
+
+    let mut error_str = String::new();
+    error_str.push_str("\"{}\" is not a valid ");
+    error_str.push_str(stripped_name);
+    error_str.push_str(" type");
+
+    let tokens = quote::quote! {
+        #[doc = "Error struct when parsing [`"]
+        #[doc = #stripped_name]
+        #[doc = "`] from string fails"]
+        #[derive(Debug)]
+        pub struct #name {
+            original: String,
+        }
+
+        impl std::fmt::Display for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, #error_str, self.original)
+            }
+        }
+    };
+
+    tokens.into()
+}
