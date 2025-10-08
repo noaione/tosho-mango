1f7f63ad2fd32c9781faf0fb19bffe9c612c295c
noaione <noaione0809@gmail.com>
2025-10-08 21:43:58 +0700

    feat(nids): add download type information

--- tosho_nids/src/models/enums.rs
+++ tosho_nids/src/models/enums.rs
@@ -52,3 +52,56 @@ impl std::fmt::Display for SaleStatus {
         }
     }
 }
+
+/// The issue download type if available.
+///
+/// ```rust
+/// use tosho_nids::models::DownloadType;
+///
+/// let dt = DownloadType::ForSale;
+/// assert_eq!(dt.to_string(), "for-sale");
+/// assert_eq!(dt.to_name(), "ForSale");
+///
+/// let parsed = "post-sale".parse::<SaleStatus>().unwrap();
+/// assert_eq!(parsed, SaleStatus::PostSale);
+/// ```
+#[derive(Debug, Clone, Copy, PartialEq, SerializeEnum, DeserializeEnum, EnumName)]
+pub enum DownloadType {
+    /// The issue is not available for download.
+    Unavailable,
+    /// The issue is available for download DRM-free.
+    DRMFree,
+    /// The issue is available for download with visual watermarks.
+    Watermarked,
+    /// The issue is available for download with invisible watermarks.
+    InvisibleWatermarked,
+}
+
+enum_error!(DownloadTypeFromStrError);
+
+impl FromStr for DownloadType {
+    type Err = DownloadTypeFromStrError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_ascii_lowercase().as_str() {
+            "unavailable" => Ok(DownloadType::Unavailable),
+            "drm-free" => Ok(DownloadType::DRMFree),
+            "visible-watermark" => Ok(DownloadType::Watermarked),
+            "invisible-watermark" => Ok(DownloadType::InvisibleWatermarked),
+            _ => Err(DownloadTypeFromStrError {
+                original: s.to_string(),
+            }),
+        }
+    }
+}
+
+impl std::fmt::Display for DownloadType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DownloadType::Unavailable => write!(f, "unavailable"),
+            DownloadType::DRMFree => write!(f, "drm-free"),
+            DownloadType::Watermarked => write!(f, "visible-watermark"),
+            DownloadType::InvisibleWatermarked => write!(f, "invisible-watermark"),
+        }
+    }
+}
